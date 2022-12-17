9f992f2bdb533891c3f5b880ab5dcc2f2cb8528e
Nesssu <80637080+Nesssu@users.noreply.github.com>
2022-12-17 17:20:11 +0200

    changed the port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
-const port: number = 8000;
+const port: number = 3000;
 
 let vehicles: {model: string, color: string, year: number, power: number}[] = [];
 
